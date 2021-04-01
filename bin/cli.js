acc3438a8a439ac6441debc0755db7d941015718
Warren Galyen <wgalyen@bellhelmets.com>
2021-04-01 09:07:56 -0700

    Refactor and Cleanup code

--- bin/cli.js
+++ bin/cli.js
@@ -6,17 +6,17 @@ const yargs = require('yargs')
 const argv = yargs
   .usage('\nAutomated Accessibility Audit\n\nUsage: a11y-audit --options')
   .options({
+    actions: {
+      alias: 'a',
+      describe: 'Test Actions',
+      type: 'array',
+      default: []
+    },
     config: {
       alias: 'c',
       describe: 'Path to Configuration File',
       type: 'string',
       default: null
-    },
-    debug: {
-      alias: 'd',
-      describe: 'Output Debug Messages',
-      type: 'boolean',
-      default: false
     },
     format: {
       alias: 'f',
@@ -35,6 +35,12 @@ const argv = yargs
       describe: 'Include Notices in Report',
       type: 'boolean',
       default: true
+    },
+    open: {
+      alias: 'O',
+      describe: 'Open Report after Creation',
+      type: 'boolean',
+      default: false
     },
     output: {
       alias: 'o',
@@ -73,18 +79,24 @@ const argv = yargs
       default: true
     }
   })
+  .command('*', 'Run A11Y Tests using Options')
+  .command('auth', 'Generate Authentication Token')
   .example('a11y-audit -c /path/to/config.json', 'Run Test using Provided Config')
-  .example('a11y-audit https://mywebsite.com', 'Run Test on URL')
+  .example('a11y-audit https://mywebsite.com --open', 'Run Test on URL')
   .example(' ', ' ')
   .example('----------------------------------', '------------------------------------------')
-  .example('NEED MORE HELP ?', 'https://bit.ly/sfcc-cli-help')
+  .example('NEED MORE HELP ?', 'https://bit.ly/a11y-audit-help')
   .example('----------------------------------', '------------------------------------------')
   .help()
   .version().argv
 
+const command = argv._[0]
+
 // Check if we passed in either a URL or a config file
 if (argv._.length === 0 && !argv.config) {
   yargs.showHelp()
+} else if (command === 'auth') {
+  require(path.join(__dirname, '../commands/auth.js'))(argv)
 } else {
   require(path.join(__dirname, '../commands/a11y.js'))(argv)
 }
